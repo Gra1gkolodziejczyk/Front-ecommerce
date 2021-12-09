bb38be092881407ca02ed5554a5076c639719aa1
Graig <graig.kolodziejczyk@gmail.com>
2021-12-09 13:28:29 +0100

    all files

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavbarWrapper } from './Navbar.style'
+//import Image from 'next/image';
+
+export const Navbar = () => {
+    return (
+        <>
+        <NavbarWrapper>
+            <h1>Mars</h1>
+        </NavbarWrapper>
+        </>
+    )
+}
+
+export default Navbar
---
+++ components/Navbar/Navbar.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const NavbarWrapper = styled.div`
+    background-color: blue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
