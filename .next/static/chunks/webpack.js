404d6e854972526b2b3280bfb4b79a6f0c5a2b21
FrekiManagarm <mathieu.chambaud@ynov.com>
2021-12-15 20:08:47 +0100

    add some packages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a71725d2642f8dc6"; }
+/******/ 		__webpack_require__.h = function() { return "a8a33176ad09a728"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -1,3 +1,46 @@
+const nextConfig = {
+  env: {
+    LOCAL_DATA_LARAVEL: process.env.LOCAL_DATA_LARAVEL,
+    LOCAL_FRONT_SERVER: process.env.LOCAL_FRONT_SERVER,
+    SERVER_API_LARAVEL: process.env.SERVER_API_LARAVEL,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style\/css.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: "null-loader",
+      });
+    } else {
+
+    }
+
+    config.plugins.push(
+      new FilterWarningsPlugin({
+        exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
+      })
+    )
+    return config;
+  },
+  experimental: {
+    optionalCatchAll: true,
+  }
+}
+
+
 module.exports = {
   reactStrictMode: true,
 }
