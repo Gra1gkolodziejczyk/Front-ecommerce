34b4b29733d6714cda15f021b1bfa3213a43e560
Graig KOLODZIEJCZYK <graig.kolodziejczyk@ynov.com>
2022-01-11 17:41:08 +0100

    footer avancement

--- container/Layout/Footer/Footer.js
+++ container/Layout/Footer/Footer.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { FooterWrapper, FooterTextWrapper, Text} from './Footer.style';
+import { FooterWrapper, FooterTitleWrapper, Title, Text, Image, FooterMediumWrapper,
+    TextOpacity, FooterLowerWrapper, FooterLogoWrapper, Logo,
+    FooterTextOpacityWrapper, FooterContentWrapper} from './Footer.style';
 
 const Footer = () => {
     return (
         <FooterWrapper>
-           <FooterTextWrapper>
-               <Text>Hello World</Text>
-           </FooterTextWrapper>
+           <FooterTitleWrapper>
+               <Title>MarsHighTech@gmail.com</Title>
+           </FooterTitleWrapper>
+            <FooterMediumWrapper>
+                <FooterContentWrapper>
+                    <Image src={""}/>
+                    <Text>102 cours Franklin Roosvelt 33000 Bordeaux</Text>
+                </FooterContentWrapper>
+                <FooterContentWrapper>
+                    <Image src={""}/>
+                    <Text>+33.7.35.55.74.32</Text>
+                </FooterContentWrapper>
+                <FooterContentWrapper>
+                    <Image src={""}/>
+                    <Text>Lorem autem uptimus prime de el famoso dif</Text>
+                </FooterContentWrapper>
+            </FooterMediumWrapper>
+            <FooterLowerWrapper>
+                <FooterTextOpacityWrapper>
+                    <TextOpacity>©All Right reserved</TextOpacity>
+                </FooterTextOpacityWrapper>
+                <FooterLogoWrapper>
+                    <Logo src={""}/>
+                    <Logo src={""}/>
+                    <Logo src={""}/>
+                    <Logo src={""}/>
+                </FooterLogoWrapper>
+            </FooterLowerWrapper>
         </FooterWrapper>
     )
 }
